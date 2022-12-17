1472d6e585a2bc680501cef420282c63ba328e0f
kden <ks230den@gmail.com>
2022-12-17 21:25:01 +0300

    Изменены строки подключения для продакшна

--- UI/src/environments/environment.prod.ts
+++ UI/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:7097',
-  angularUrl: 'http://localhost:80/',
+  apiUrl: 'http://185.225.35.229:7097',
+  angularUrl: 'http://185.225.35.229:80/',
   siteKeyReCaptha:'6Lc11kojAAAAAOO3EA2-SzzjEwAa3lZDqz_EYVzL'
 };
